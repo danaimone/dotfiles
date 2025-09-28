cde2c387b4d2c90a2f7ad114899d80127e333b4b
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2025-09-28 15:33:26 -0700

    Fix LSP deprecation warnings and add plugin cheatsheet - Update LSP config to use modern nvim-lspconfig approach - Add which-key plugin for in-editor shortcut help - Add comprehensive Neovim plugin cheatsheet to README - Fix TypeScript language server installation issues - Remove deprecated lspconfig framework warnings

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -2,38 +2,33 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup({
-        log_level = vim.log.levels.DEBUG,
-      })
+      require("mason").setup()
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = {
-      auto_install = true,
-    },
+    dependencies = { "mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "omnisharp", "ts_ls" },
+        ensure_installed = { "lua_ls", "typescript-language-server", "html" },
+        automatic_installation = true,
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
+    branch = "master",
+    dependencies = { "mason-lspconfig.nvim", "cmp-nvim-lsp" },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
 
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
+      -- Setup servers
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
 
+      -- Key mappings
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
